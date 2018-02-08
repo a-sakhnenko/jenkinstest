efeab8396a4196e331b80241dc3372bc8b741172
Aleksandr Sakhnenko <Aleksandr_Sakhnenko@epam.com>
2018-02-08 14:06:52 +0300

    Added script for build the specific branch with mailer

---
+++ src/main/groovy/jobs/firstScript.groovy
@@ -0,0 +1,39 @@
+package jobs
+
+def projectURL = 'git@github.com:a-sakhnenko/jenkinstest.git'
+def branchName = 'branch2'
+
+
+job('testing-to-build') {
+
+    scm {
+        git {
+            remote {
+                name('origin')
+                url("$projectURL")
+            }
+            branch("$branchName")
+        }
+
+    }
+    steps {
+        maven('-e clean package')
+    }
+    publishers {
+        extendedEmail {
+            attachBuildLog(true)
+            triggers {
+                failure() {
+                    sendTo {
+                        firstFailingBuildSuspects()
+                    }
+                }
+                success() {
+                    sendTo {
+                        developers()
+                    }
+                }
+            }
+        }
+    }
+}
